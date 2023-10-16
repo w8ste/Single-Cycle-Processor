1196c9bc3786a52a51fab8ba7d670987f063e3f0
Hannes Albert <hannes10042004@icloud.com>
2023-10-16 18:30:41 +0200

    Add instruction memory module

---
+++ main/instrmem.sv
@@ -0,0 +1,10 @@
+module instrmem(input logic [31:0] A,
+		output logic [31:0] RD);
+
+   logic [31:0] ram [63:0];
+
+   initial
+     $readmemh("memfile.dat",ram);
+   assign RD = ram[A[31:2]];
+
+endmodule; // instrmem
